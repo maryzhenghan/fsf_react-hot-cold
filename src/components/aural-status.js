81d327970d1bea9525f25ee010e12b9fd6096840
Mary Han <maryzhenghan@gmail.com>
2018-09-19 12:20:57 -0700

    Update aural status code

--- src/components/aural-status.js
+++ src/components/aural-status.js
@@ -6,8 +6,7 @@ export default function AuralStatus(props) {
       id="status-readout"
       className="visuallyhidden"
       aria-live="assertive"
-      aria-atomic="true"
-    >
+      aria-atomic="true">
       {props.auralStatus}
     </p>
   );
